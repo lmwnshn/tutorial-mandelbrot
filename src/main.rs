5401f5a3238d73558f96df79e18039c070d9a67c
Wan Shen Lim <wanshenl@andrew.cmu.edu>
2018-01-05 22:16:00 -0500

    parse functions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 extern crate num;
 use num::Complex;
+use std::str::FromStr;
 
 /// escape_time(c, l) : check if `c` in Mandelbrot with up to `l` iterations
 ///
 /// Returns:
 ///     `Some(i)` if `c` left within `i` iterations, `i` < `l`
 ///     `None` otherwise
-#[allow(dead_code)]
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
@@ -18,6 +18,41 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T,T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
+                (Ok(l), Ok(r)) => Some ((l,r)),
+                _ => None
+            }
+        }
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",','), None);
+    assert_eq!(parse_pair::<i32>("10",','), None);
+    assert_eq!(parse_pair::<i32>("10,",','), None);
+    assert_eq!(parse_pair::<i32>("10,20",','), Some((10,20)));
+    assert_eq!(parse_pair::<i32>("10,20x",','), None);
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"),
+               Some(Complex { re: 1.25, im: -0.0625 }));
+    assert_eq!(parse_complex(",1.0"), None)
 }
