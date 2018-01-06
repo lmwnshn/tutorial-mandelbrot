081352a7aeac6c63c234ba6bcc0bc2f0b024f9cd
Wan Shen Lim <wanshenl@andrew.cmu.edu>
2018-01-05 22:08:13 -0500

    escape_time - docstring

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 extern crate num;
 use num::Complex;
 
+/// escape_time(c, l) : check if `c` in Mandelbrot with up to `l` iterations
+///
+/// Returns:
+///     `Some(i)` if `c` left within `i` iterations, `i` < `l`
+///     `None` otherwise
 #[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>) {
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
+    for i in 0..limit {
         z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
     }
+    None
 }
 
 fn main() {
