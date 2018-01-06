0ee2be7a142de7a589075097b49373230f19ae30
Wan Shen Lim <wanshenl@andrew.cmu.edu>
2018-01-05 22:24:39 -0500

    pixel/point calculations and rendering

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,42 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
     }
 }
 
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  top_left: Complex<f64>,
+                  bot_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let tl = top_left;
+    let br = bot_right;
+    let (width, height) = (br.re - tl.re, tl.im - br.im);
+
+    Complex {
+        re: tl.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: tl.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          top_left: Complex<f64>,
+          bot_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0 .. bounds.1 {
+        for col in 0 .. bounds.0 {
+            let pt = pixel_to_point(bounds, (col, row), top_left, bot_right);
+
+            pixels[row * bounds.0 + col] =
+                match escape_time(pt, 255) {
+                    None => 0,
+                    Some(i) => 255 - i as u8
+                }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -55,4 +91,12 @@ fn test_parse_complex() {
     assert_eq!(parse_complex("1.25,-0.0625"),
                Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",1.0"), None)
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100,100), (25,75),
+                              Complex { re: -1.0, im:  1.0 },
+                              Complex { re:  1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.5 });
 }
